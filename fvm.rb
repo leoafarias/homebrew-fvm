a9ea2589faba35a64dee5c97a630c014eeca8122
cli_pkg <cli_pkg@none>
2020-10-11 15:55:50 +0000

    Update fvm to 1.3.1

--- fvm.rb
+++ fvm.rb
@@ -1,8 +1,8 @@
 class Fvm < Formula
     desc "Simple cli to manage Flutter SDK versions per project"
     homepage "https://github.com/leoafarias/fvm"
-    url "https://github.com/leoafarias/fvm/archive/v1.3.0.tar.gz"
-    sha256 "2257a70d0a2a3ae2f549079d7e73e477057b5c49b111356192446d02f9e9e69f"
+    url "https://github.com/leoafarias/fvm/archive/v1.3.1.tar.gz"
+    sha256 "b8bb1be86f251149541f2884f16f3737c2c920a977e8e8c83ddc017334bf2c10"
     license "MIT"
   
     depends_on "dart-lang/dart/dart" => :build
