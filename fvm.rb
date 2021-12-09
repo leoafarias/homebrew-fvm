6f49545ae3a57575181b35eeec8d3f145ffa29fd
leoafarias <leoafarias@gmail.com>
2021-12-09 01:45:44 -0500

    Update

--- fvm.rb
+++ fvm.rb
@@ -1,66 +1,63 @@
 require "yaml"
 
 class Fvm < Formula
-    desc "Simple cli to manage Flutter SDK versions per project"
-    homepage "https://github.com/leoafarias/fvm"
-    url "https://github.com/leoafarias/fvm/archive/2.2.5.tar.gz"
-    sha256 "6b91db9d184e54521082e67af21ef897753848c41c5375a888b5cd4f94055249"
-    license "MIT"
-  
-    depends_on "dart-lang/dart/dart" => :build
-  
-    def install
-      # Tell the pub server where these installations are coming from.
-      ENV["PUB_ENVIRONMENT"] = "homebrew:fvm"
+  desc "Simple cli to manage Flutter SDK versions per project"
+  homepage "https://github.com/leoafarias/fvm"
+  url "https://github.com/leoafarias/fvm/archive/2.2.5.tar.gz"
+  sha256 "6b91db9d184e54521082e67af21ef897753848c41c5375a888b5cd4f94055249"
+  license "MIT"
 
-      system _dart/"dart", "pub", "get"
-      # Build a native-code executable on 64-bit systems only. 32-bit Dart
-      # doesn't support this.
-      if Hardware::CPU.is_64_bit?
-        _install_native_executable
-      else
-        _install_script_snapshot
-      end
-      chmod 0555, "#{bin}/fvm"
-    end
+  depends_on "dart-lang/dart/dart" => :build
 
-    test do
-      system "false"
-    end
-      
-    private
+  def install
+    # Tell the pub server where these installations are coming from.
+    ENV["PUB_ENVIRONMENT"] = "homebrew:fvm"
 
-    def _dart
-      @_dart ||= Formula["dart-lang/dart/dart"].libexec/"bin"
+    system _dart/"dart", "pub", "get"
+    # Build a native-code executable on 64-bit systems only. 32-bit Dart
+    # doesn't support this.
+    if Hardware::CPU.is_64_bit?
+      _install_native_executable
+    else
+      _install_script_snapshot
     end
-  
-    def _version
-      @_version ||= YAML.safe_load(File.read("pubspec.yaml"))["version"]
-    end
+    chmod 0555, "#{bin}/fvm"
+  end
+
+  test do
+    system "false"
+  end
+
+  private
+
+  def _dart
+    @_dart ||= Formula["dart-lang/dart/dart"].libexec/"bin"
+  end
+
+  def _version
+    @_version ||= YAML.safe_load(File.read("pubspec.yaml"))["version"]
+  end
+
+  def _install_native_executable
+    system _dart/"dart", "compile", "exe", "-Dversion=#{_version}",
+           "bin/main.dart", "-o", "fvm"
+    bin.install "fvm"
+  end
+
+  def _install_script_snapshot
+    system _dart/"dart", "compile", "jit-snapshot",
+           "-Dversion=#{_version}",
+           "-o", "main.dart.app.snapshot",
+           "bin/main.dart"
+    lib.install "main.dart.app.snapshot"
+
     
-    def _install_native_executable
-      system _dart/"dart", "compile", "exe", "-Dversion=#{_version}",
-             "bin/main.dart", "-o", "fvm"
-      bin.install "fvm"
-    end
-    
-    def _install_script_snapshot
-      system _dart/"dart", "compile", "jit-snapshot",
-            "-Dversion=#{_version}",
-            "-o", "main.dart.app.snapshot",
-            "bin/main.dart"
-      lib.install "main.dart.app.snapshot"
+    cp _dart/"dart", lib
 
-      # Copy the version of the Dart VM we used into our lib directory so that if
-      # the user upgrades their Dart VM version it doesn't break Sass's snapshot,
-      # which was compiled with an older version.
-      cp _dart/"dart", lib
-
-      (bin/"fvm").write <<~SH
-        #!/bin/sh
-        exec "#{lib}/dart" "#{lib}/main.dart.app.snapshot" "$@"
-      SH
-
-    end
+    (bin/"fvm").write <<~SH
+      #!/bin/sh
+      exec "#{lib}/dart" "#{lib}/main.dart.app.snapshot" "$@"
+    SH
+  end
 end
   
