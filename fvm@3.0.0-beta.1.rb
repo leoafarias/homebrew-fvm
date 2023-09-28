b5565e9c1d935e221adc014024a1fc0310e9ccdf
cli_pkg <cli_pkg@none>
2023-09-28 18:00:46 -0400

    Add a formula for fvm 3.0.0-beta.1

---
+++ fvm@3.0.0-beta.1.rb
@@ -0,0 +1,63 @@
+require "yaml"
+
+class FvmAT300Beta1 < Formula
+  desc "Simple cli to manage Flutter SDK versions per project"
+  homepage "https://github.com/fluttertools/fvm"
+  url "https://github.com/fluttertools/fvm/archive/3.0.0-beta.1.tar.gz"
+  sha256 "f65bae30cecffe5b531ffa415f99fa8949dd4eb9cc3b4cce8a9fc84d7cbf335e"
+  license "MIT"
+
+  depends_on "dart-lang/dart/dart" => :build
+
+  def install
+    # Tell the pub server where these installations are coming from.
+    ENV["PUB_ENVIRONMENT"] = "homebrew:fvm"
+
+    system _dart/"dart", "pub", "get"
+    # Build a native-code executable on 64-bit systems only. 32-bit Dart
+    # doesn't support this.
+    if Hardware::CPU.is_64_bit?
+      _install_native_executable
+    else
+      _install_script_snapshot
+    end
+    chmod 0555, "#{bin}/fvm"
+  end
+
+  test do
+    system "false"
+  end
+
+  private
+
+  def _dart
+    @_dart ||= Formula["dart-lang/dart/dart"].libexec/"bin"
+  end
+
+  def _version
+    @_version ||= YAML.safe_load(File.read("pubspec.yaml"))["version"]
+  end
+
+  def _install_native_executable
+    system _dart/"dart", "compile", "exe", "-Dversion=#{_version}",
+           "bin/main.dart", "-o", "fvm"
+    bin.install "fvm"
+  end
+
+  def _install_script_snapshot
+    system _dart/"dart", "compile", "jit-snapshot",
+           "-Dversion=#{_version}",
+           "-o", "main.dart.app.snapshot",
+           "bin/main.dart"
+    lib.install "main.dart.app.snapshot"
+
+    
+    cp _dart/"dart", lib
+
+    (bin/"fvm").write <<~SH
+      #!/bin/sh
+      exec "#{lib}/dart" "#{lib}/main.dart.app.snapshot" "$@"
+    SH
+  end
+end
+  
