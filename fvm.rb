3567cba13501934bdc0c96b5bea7eb3f860f75db
Leo Farias <leoafarias@gmail.com>
2024-02-12 10:19:48 -0500

    Forced sdk version on homebrew

--- fvm.rb
+++ fvm.rb
@@ -7,19 +7,37 @@ class Fvm < Formula
   sha256 "fffb16f1a8ec109a62cd128836f4a4ac79263b165e5fd770cf5503c96c5d7473"
   license "MIT"
 
-  depends_on "dart-lang/dart/dart" => :build
+  # Determine architecture and set the Dart SDK resource accordingly
+  dart_sdk_url, dart_sdk_sha = if Hardware::CPU.intel?
+    ["https://storage.googleapis.com/dart-archive/channels/stable/release/3.2.6/sdk/dartsdk-macos-x64-release.zip",
+     "97661f20230686381f4fc5b05a63c6b5d5abc9570bf93ad4e5fc09309cd98517"]
+  elsif Hardware::CPU.arm?
+    ["https://storage.googleapis.com/dart-archive/channels/stable/release/3.2.6/sdk/dartsdk-macos-arm64-release.zip",
+     "2e04c91039f9cc05b2e93ce788aa1ce08bc4df5b50c84d6b4e21ba2b2538adb6"]
+  end
+
+  resource "dart-sdk" do
+    url dart_sdk_url
+    sha256 dart_sdk_sha
+  end
 
   def install
-    # Tell the pub server where these installations are coming from.
-    ENV["PUB_ENVIRONMENT"] = "homebrew:fvm"
+    # Resource installation for Dart SDK
+    resource("dart-sdk").stage do
+      libexec.install Dir["*"] # Assumes Dart SDK zip layout matches what's expected
+    end
 
-    system _dart/"dart", "pub", "get"
-    # Build a native-code executable on 64-bit systems only. 32-bit Dart
-    # doesn't support this.
+    ENV["PUB_ENVIRONMENT"] = "homebrew:fvm"
+    
+    # Adjust paths to use the vendored Dart SDK
+    dart = libexec/"bin/dart"
+    
+    system dart, "pub", "get"
+    
     if Hardware::CPU.is_64_bit?
-      _install_native_executable
+      _install_native_executable(dart)
     else
-      _install_script_snapshot
+      _install_script_snapshot(dart)
     end
     chmod 0555, "#{bin}/fvm"
   end
@@ -30,29 +48,24 @@ class Fvm < Formula
 
   private
 
-  def _dart
-    @_dart ||= Formula["dart-lang/dart/dart"].libexec/"bin"
-  end
-
   def _version
     @_version ||= YAML.safe_load(File.read("pubspec.yaml"))["version"]
   end
 
-  def _install_native_executable
-    system _dart/"dart", "compile", "exe", "-Dversion=#{_version}",
+  def _install_native_executable(dart)
+    system dart, "compile", "exe", "-Dversion=#{_version}",
            "bin/main.dart", "-o", "fvm"
     bin.install "fvm"
   end
 
-  def _install_script_snapshot
-    system _dart/"dart", "compile", "jit-snapshot",
+  def _install_script_snapshot(dart)
+    system dart, "compile", "jit-snapshot",
            "-Dversion=#{_version}",
            "-o", "main.dart.app.snapshot",
            "bin/main.dart"
     lib.install "main.dart.app.snapshot"
-
     
-    cp _dart/"dart", lib
+    cp dart, lib
 
     (bin/"fvm").write <<~SH
       #!/bin/sh
@@ -60,4 +73,3 @@ class Fvm < Formula
     SH
   end
 end
-  
