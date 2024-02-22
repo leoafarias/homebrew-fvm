df80fd05ead68b63f5eabed86d2a694e76e04226
cli_pkg <cli_pkg@none>
2024-02-22 14:23:09 +0000

    Update fvm to 3.0.8

--- fvm.rb
+++ fvm.rb
@@ -3,8 +3,8 @@ require "yaml"
 class Fvm < Formula
   desc "Simple cli to manage Flutter SDK versions per project"
   homepage "https://github.com/leoafarias/fvm"
-  url "https://github.com/leoafarias/fvm/archive/3.0.7.tar.gz"
-  sha256 "fc00a8b7f99ed5c63cb3dfc55722cb8b3feea739391706ee6f5b664f60854859"
+  url "https://github.com/leoafarias/fvm/archive/3.0.8.tar.gz"
+  sha256 "91fe72257f667fbf3b058ee97487d6efd1aa415d29ff60770af9454e91d47ace"
   license "MIT"
 
   # Determine architecture and set the Dart SDK resource accordingly
