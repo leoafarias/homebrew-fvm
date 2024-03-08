7ffe88d8cd731e49333d33a8aebd304b2b445906
Leo Farias <leoafarias@gmail.com>
2024-03-08 10:12:19 -0500

    Update sdk

--- fvm@2.4.1.rb
+++ fvm@2.4.1.rb
@@ -9,17 +9,17 @@ class FvmAT241 < Formula
 
   # Determine architecture and set the Dart SDK resource accordingly
   dart_sdk_url, dart_sdk_sha = if OS.mac? && Hardware::CPU.intel?
-    ["https://storage.googleapis.com/dart-archive/channels/stable/release/3.2.6/sdk/dartsdk-macos-x64-release.zip",
-     "97661f20230686381f4fc5b05a63c6b5d5abc9570bf93ad4e5fc09309cd98517"]
+    ["https://storage.googleapis.com/dart-archive/channels/stable/release/2.17.7/sdk/dartsdk-macos-x64-release.zip",
+     "ba258fff40822cb410c4f1f7916b63f0837903a6bae8f4bd83341053b10ecbe3"]
   elsif OS.mac? && Hardware::CPU.arm?
-    ["https://storage.googleapis.com/dart-archive/channels/stable/release/3.2.6/sdk/dartsdk-macos-arm64-release.zip",
-     "2e04c91039f9cc05b2e93ce788aa1ce08bc4df5b50c84d6b4e21ba2b2538adb6"]
+    ["https://storage.googleapis.com/dart-archive/channels/stable/release/2.17.7/sdk/dartsdk-macos-arm64-release.zip",
+     "a4be379202cf731c7e33de20b4abc4ca1e2e726bc5973222b3a7ae5a0cabfce1"]
   elsif OS.linux? && Hardware::CPU.intel?
-    ["https://storage.googleapis.com/dart-archive/channels/stable/release/3.2.6/sdk/dartsdk-linux-x64-release.zip",
-     "253390a14f6f5d764c82df4b2c2cf18a1c30a8e1fe0849448cc4fedabaaf1d48"]
+    ["https://storage.googleapis.com/dart-archive/channels/stable/release/2.17.7/sdk/dartsdk-linux-x64-release.zip",
+     "ba8bc85883e38709351f78c527cbf72e22cd234b3678a1ec6a2e781f7984e624"]
   elsif OS.linux? && Hardware::CPU.arm?
-    ["https://storage.googleapis.com/dart-archive/channels/stable/release/3.2.6/sdk/dartsdk-linux-arm64-release.zip",
-     "9818a37dd39e8e91a0159bdd2522213f9d36bbd99b715465b4606190e6ae41c3"]
+    ["https://storage.googleapis.com/dart-archive/channels/stable/release/2.17.7/sdk/dartsdk-linux-arm64-release.zip",
+     "ee0acf66e184629b69943e9f29459e586095895c6aae6677ab027d99c00934b6"]
   end
 
   resource "dart-sdk" do
